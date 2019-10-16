d2831b7833eadbfd2c4dab62f0ba6f7565a27118
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-10-16 21:06:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,7 +32,9 @@ class Song
     genre_count = Hash.new
     @@genres.each do |genre|
       if !genre_count.has_key?(genre)
-        genre_count[genre] = 1 
+        genre_count[genre] = 1
+      else
+        genre_count[genre] += 1 
     end
   end
 
